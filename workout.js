c96d6e4f32bc599b6b7906f37c81629b9f633b1c
DrewRowan <drew.r@ministryofsoup.com>
2021-07-15 18:14:42 +0200

    Add files via upload

---
+++ workout.js
@@ -0,0 +1,20 @@
+const workoutJson = {"list": 
+    [
+        {"name": "Foam Roll Calves", "type": "time", "amount": 45, "rest": 10},
+        {"name": "Foam Roll Hammys", "type": "none", "amount": ""},
+        {"name": "Foam Roll Glutes", "type": "none", "amount": ""},
+        {"name": "Foam Roll Quads", "type": "none", "amount": ""},
+        {"name": "Calf Raises (2 up, 1 down)", "type": "rep", "amount": 15},
+        {"name": "Bridge walks", "type": "time", "amount": 45, "rest": 10},
+        {"name": "Narrow Squat", "type": "rep", "amount": 15},
+        {"name": "Hip Opener", "type": "time", "amount": 45, "rest": 10},
+        {"name": "Pike", "type": "rep", "amount": 15},
+        {"name": "Clams", "type": "rep", "amount": 15},
+        {"name": "Glute Bridges", "type": "rep", "amount": 15},
+        {"name": "Side Leg Raise", "type": "rep", "amount": 15},
+        {"name": "Side Bum Stretch", "type": "time", "amount": 45, "rest": 10},
+        {"name": "Hamstring Stretch", "type": "time", "amount": 45, "rest": 10},
+        {"name": "Plank", "type": "time", "amount": 45},
+        {"name": "Quad Stretch", "type": "time", "amount": 45}
+    ]
+}
