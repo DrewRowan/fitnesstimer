48c1fbe32d2357db751674284b9a264cb5323851
Drew Rowan <andrew.rowan@wallee.com>
2021-11-16 09:45:01 +0100

    add copy row functionality

--- form.js
+++ form.js
@@ -4,22 +4,23 @@ const currentUrl = window.location.href.split(/[?#]/)[0];
 // elements
 let exerciseTypeSelect = document.getElementsByClassName("exercise-type");
 let removeRowButton = document.getElementsByClassName("fa-trash");
+let copyRowButton = document.getElementsByClassName("bi-clipboard");
+
 let addRowButton = document.getElementById("add-row");
 let saveWorkoutButton = document.getElementById("submit");
 let workoutListForm = document.getElementById("workout-list");
 let navBar = document.getElementById("navbarToggleExternalContent");
-let newRow = '<li><div class="form-row mb-1"><div class="col-1"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="height:30px;" class="svg-inline--fa fa-trash fa-w-14 fa-lg"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z" class=""></path></svg><i class="sort-by-desc"></i><i class="sort-by-asc"></i></div><div class="col"><span class="ui-icon ui-icon-arrowthick-2-n-s"><input type="text" class="form-control" placeholder="Exercise name" name="name[]"></div><div class="col"><select id="selectbox" class="form-control exercise-type" name="type[]"><option value="" disabled selected>Choose type...</option><option value="none">General</option><option value="rep">Rep</option><option value="time">Time</option></select></div><div class="col"><input type="number" class="form-control d-none amount" placeholder="" name="amount[]"></div><div class="col"><input type="number" class="form-control d-none rest" placeholder="Rest Time" name="rest[]"></div></div><li>';
+let newRow = '<li><div class="form-row mb-1"><div class="col-1"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16"><path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/><path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/></svg><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" style="height:30px;" class="svg-inline--fa fa-trash fa-w-14 fa-lg"><path fill="currentColor" d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z" class=""></path></svg><i class="sort-by-desc"></i><i class="sort-by-asc"></i></div><div class="col"><span class="ui-icon ui-icon-arrowthick-2-n-s"><input type="text" class="form-control" placeholder="Exercise name" name="name[]"></div><div class="col"><select id="selectbox" class="form-control exercise-type" name="type[]"><option value="" disabled selected>Choose type...</option><option value="none">General</option><option value="rep">Rep</option><option value="time">Time</option></select></div><div class="col"><input type="number" class="form-control d-none amount" placeholder="" name="amount[]"></div><div class="col"><input type="number" class="form-control d-none rest" placeholder="Rest Time" name="rest[]"></div></div><li>';
 
 let names;
 let types;
 let amounts;
 let rests;
 
-addRowButton.addEventListener("click", duplicateRow);
+addRowButton.addEventListener("click", addRow);
 saveWorkoutButton.addEventListener("click", saveWorkout);
 // actions
-addMultipleListeners(exerciseTypeSelect, "change", initSelectType);
-addMultipleListeners(removeRowButton, "click", deleteRow);
+attachEventListeners();
 populateForm();
 
 // functions
@@ -115,10 +116,23 @@ function checkValidation() {
     checkAndSetInvalid(this);
 }
 
-function duplicateRow() {
-    workoutListForm.insertAdjacentHTML('beforeend', newRow);
+function attachEventListeners() {
     addMultipleListeners(exerciseTypeSelect, "change", initSelectType);
     addMultipleListeners(removeRowButton, "click", deleteRow);
+    addMultipleListeners(copyRowButton, "click", copyRow);
+}
+
+function addRow() {
+    workoutListForm.insertAdjacentHTML('beforeend', newRow);
+    attachEventListeners();
+}
+
+function copyRow() {
+    let parent = this.closest("li");
+    var clone = parent.cloneNode(true);
+    //need to add a way to copy select box values
+    parent.after(clone);
+    attachEventListeners();
 }
 
 function initSelectType() {
@@ -164,8 +178,7 @@ function populateForm() {
         Array.from(exerciseTypeSelect).forEach(function(list) {
             selectType(list);
         });
-        addMultipleListeners(exerciseTypeSelect, "change", initSelectType);
-        addMultipleListeners(removeRowButton, "click", deleteRow);
+        attachEventListeners();
         exerciseNext.innerText = "First:" + workoutJsonList[0].name;
     } else {
         navBar.classList.add("show");
